d880c193c0b83c0c2f05759b320ee1df25a2cecf
imkrish <imkrish.developer@gmail.com>
2017-11-13 22:07:31 +1300

    delete IController

--- src/api/interfaces/IController.ts
+++
@@ -1,8 +0,0 @@
-export interface IController {
-    getAll: (req: Request, res: Response) => void
-    createOne: (req: Request, res: Response) => void
-    getOne: (req: Request, res: Response) => void
-    updateOne: (req: Request, res: Response) => void
-    deleteOne: (req: Request, res: Response) => void
-    findById: (req: Request, res: Response) => void
-}
